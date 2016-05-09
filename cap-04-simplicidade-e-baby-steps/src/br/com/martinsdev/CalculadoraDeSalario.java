5b11bc0d526f2f67bf64e2153081c6544d47678e
Gabriel Angelo <gabriel.angelo91@gmail.com>
2016-05-09 09:50:40 -0300

    Capitulo 04: Simplicidade e Baby Steps

---
+++ cap-04-simplicidade-e-baby-steps/src/br/com/martinsdev/CalculadoraDeSalario.java
@@ -0,0 +1,15 @@
+package br.com.martinsdev;
+
+/**
+ * Created by gabriel on 5/5/16.
+ */
+public class CalculadoraDeSalario {
+    public double calculaSalario(Funcionario funcionario) {
+
+        if (funcionario.getSalario() > 3000) {
+            return funcionario.getSalario() * 0.8;
+        }
+
+        return funcionario.getSalario() * 0.9;
+    }
+}
---
+++ cap-04-simplicidade-e-baby-steps/src/br/com/martinsdev/Cargo.java
@@ -0,0 +1,10 @@
+package br.com.martinsdev;
+
+/**
+ * Created by gabriel on 5/5/16.
+ */
+public enum Cargo {
+    DESENVOLVEDOR,
+    DBA,
+    TESTADOR
+}
